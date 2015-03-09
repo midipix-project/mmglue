3c3117746510caa0caaf09c4142ad2b60e814759
midipix <writeonce@midipix.org>
2015-03-08 22:22:34 -0400

    dynlink.c: replace stubs with calls into the ldso vtable.
    signed-off by Z. Gilboa; see copying.midipix (9cd0746c) for additional information.

--- src/ldso/nt64/dynlink.c
+++ src/ldso/nt64/dynlink.c
@@ -1,40 +1,44 @@
 #define _BSD_SOURCE
 
 #include <dlfcn.h>
+#include "psxglue.h"
 #include "pthread_impl.h"
+
+extern struct __ldso_vtbl * __ldso_vtbl;
 
 int __dladdr(const void * addr, Dl_info * info)
 {
-	return 0;
+	return __ldso_vtbl->dladdr(addr,info);
 }
 
 int __dlinfo(void * dso, int req, void * res)
 {
-	return 0;
+	return __ldso_vtbl->dlinfo(dso,req,res);
 }
 
 void *__dlsym(void * restrict p, const char * restrict s, void * restrict ra)
 {
-	return 0;
+	return __ldso_vtbl->dlsym(p,s,ra);
 }
 
 void * dlopen(const char * file, int mode)
 {
-	return 0;
+	return __ldso_vtbl->dlopen(file,mode);
 }
 
 int dlclose(void *p)
 {
-        return 0;
+        return __ldso_vtbl->dlclose(p);
 }
 
 char * dlerror(void)
 {
-	return 0;
+	return __ldso_vtbl->dlerror();
 }
 
 void __reset_tls(void)
 {
+	__ldso_vtbl->reset_tls();
 }
 
 void *__copy_tls(unsigned char * mem)
