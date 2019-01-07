bfd5032fecd9704514c27f4021c8e084df188710
midipix <writeonce@midipix.org>
2019-01-07 06:41:14 -0500

    project: added libc.so's linking flags.

--- project/extras.mk
+++ project/extras.mk
@@ -1,2 +1,18 @@
 ./src/internal/version.o:	build/version.h
 ./src/internal/version.lo:	build/version.h
+
+# libc.so
+SHARED_OBJS      += $(LDSO_OBJS)
+
+LDFLAGS_COMMON   += -nostdlib
+LDFLAGS_COMMON   += -Wl,-Bsymbolic-functions
+LDFLAGS_COMMON   += -Wl,--sort-common
+LDFLAGS_COMMON   += -Wl,--gc-sections
+LDFLAGS_COMMON   += -Wl,--no-undefined
+LDFLAGS_COMMON   += -Wl,--exclude-libs=ALL
+
+LDFLAGS_COMMON   += -Wl,-e              -Wl,_dlstart
+LDFLAGS_COMMON   += -Wl,--hash-style    -Wl,both
+LDFLAGS_COMMON   += -Wl,--sort-section  -Wl,alignment
+
+SHARED_LIB_DEPS  += -lgcc -lgcc_eh
