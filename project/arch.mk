fb33f19fdd71812fb5cc1071aec2c6273d56b8e0
midipix <writeonce@midipix.org>
2019-01-07 09:51:18 -0500

    mmglue: libc.so: ldflags: added --exclude-symbols definitions.

--- project/arch.mk
+++ project/arch.mk
@@ -2,4 +2,19 @@ ifeq ($(OS),midipix)
 
 src/env/__libc_start_main.lo: CFLAGS_SHARED += -USHARED
 
+LDFLAGS_CONFIG += -Wl,--exclude-symbols=_dlstart
+LDFLAGS_CONFIG += -Wl,--exclude-symbols=__syscall_vtbl
+LDFLAGS_CONFIG += -Wl,--exclude-symbols=__ldso_vtbl
+LDFLAGS_CONFIG += -Wl,--exclude-symbols=__psx_vtbl
+LDFLAGS_CONFIG += -Wl,--exclude-symbols=__teb_sys_idx
+LDFLAGS_CONFIG += -Wl,--exclude-symbols=__teb_libc_idx
+LDFLAGS_CONFIG += -Wl,--exclude-symbols=__vm_lock_impl
+LDFLAGS_CONFIG += -Wl,--exclude-symbols=__vm_unlock_impl
+LDFLAGS_CONFIG += -Wl,--exclude-symbols=_IO_feof_unlocked
+LDFLAGS_CONFIG += -Wl,--exclude-symbols=_IO_ferror_unlocked
+LDFLAGS_CONFIG += -Wl,--exclude-symbols=_IO_getc
+LDFLAGS_CONFIG += -Wl,--exclude-symbols=_IO_getc_unlocked
+LDFLAGS_CONFIG += -Wl,--exclude-symbols=_IO_putc
+LDFLAGS_CONFIG += -Wl,--exclude-symbols=_IO_putc_unlocked
+
 endif
