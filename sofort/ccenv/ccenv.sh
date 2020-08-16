2963f0a120764a00e5dcbe01e8c6bd737d02c3be
midipix <writeonce@midipix.org>
2020-08-16 08:51:24 +0000

    build system: ccenv: ccenv_set_cc_bits(): further accommodate legacy shells.

--- sofort/ccenv/ccenv.sh
+++ sofort/ccenv/ccenv.sh
@@ -640,7 +640,7 @@ ccenv_set_cc_bits()
 	ccenv_internal_test='char x[(sizeof(%s) == %s/8) ? 1 : -1];'
 
 	for ccenv_internal_guess in 64 32 128; do
-		if [ -z $ccenv_internal_size ]; then
+		if [ -z "${ccenv_internal_size:-}" ]; then
 			ccenv_internal_str=$(printf "$ccenv_internal_test"  \
 				"$ccenv_internal_type"                      \
 				"$ccenv_internal_guess")
