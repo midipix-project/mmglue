7581176d05ea0003d1159f7eea3cd92597db8893
midipix <writeonce@midipix.org>
2021-04-10 10:31:23 +0000

    build system: posix make support: refactored handling of the PACKAGE_APP macro.

--- sofort/core/_flavor/flavor_app_linking_all_shared.mk
+++ sofort/core/_flavor/flavor_app_linking_all_shared.mk
@@ -1,7 +1,7 @@
 # flavor_app_linking_all_shared.mk: handling of frontend linking options.
 # this file is covered by COPYING.SOFORT.
 
+PACKAGE_APP   = $(SHARED_APP)
+
 package-app:	shared-app
-app:		PACKAGE_APP = $(SHARED_APP)
-app-tag:	PACKAGE_APP = $(SHARED_APP)
 app.tag:	$(SHARED_APP)
--- sofort/core/_flavor/flavor_app_linking_default.mk
+++ sofort/core/_flavor/flavor_app_linking_default.mk
@@ -1,7 +1,7 @@
 # flavor_app_linking_default.mk: handling of frontend linking options.
 # this file is covered by COPYING.SOFORT.
 
+PACKAGE_APP   = $(DEFAULT_APP)
+
 package-app:	default-app
-app:		PACKAGE_APP = $(DEFAULT_APP)
-app-tag:	PACKAGE_APP = $(DEFAULT_APP)
 app.tag:	$(DEFAULT_APP)
